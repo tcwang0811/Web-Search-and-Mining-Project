ea4cd16b8a8f57420a6600556c175df185008908
tcwang0811 <tcwang0811@gmail.com>
2018-04-07 20:18:03 +0800

    Documents input and Create files

---
+++ Project1/Similarity.py
@@ -0,0 +1,9 @@
+import util
+
+# ------------------------------------------------- #
+
+# input: a document vector and query vector (tf or tf-idf)
+# output: the similarity of two vector
+# two method: Cosine and Euclidean Distance
+
+# ------------------------------------------------- #
---
+++ Project1/main.py
@@ -0,0 +1,35 @@
+import os
+import VectorSpaceModel
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    # open and store document
+    path = "D:/PycharmProjects/WSMProject/documents/"
+
+    documents = []
+    documentsNo = []
+
+    for file in os.listdir(path):
+        if file.endswith(".product"):
+            thisFile = open(path + file, 'r')
+            documents.append(thisFile.read())
+            documentsNo.append(file)
+            thisFile.close()
+
+    query = "drill wood sharp"
+
+
+
+
+if __name__ == "__main__":
+    main()
