751d54b50fe84d1964e57840a4e2ce04e6825bc9
tcwang0811 <tcwang0811@gmail.com>
2018-05-12 21:28:20 +0800

    Add query and read data in class

--- Project2/Models.py
+++ Project2/Models.py
@@ -1,15 +1,95 @@
 import math
 import numpy as np
+from PorterStemmer import PorterStemmer
 
 
 class Model:
+    # self.__index = file index
+    # self.__lenDict = length of every document
+    # self.__totalTermAndDoc = total number of term and number of document
+    # self.__uniqueTerm = number of unique term
+    # self.__ID = internal and external ID mapping
+    # self.__query = query number and content
+    # self.__stemmer = stemmer
 
-    def __init__(self, inputData, lengthDict, termAndDoc, uniqueT, ID_mapping):
-        self.__index = inputData
-        self.__lenDict = lengthDict
-        self.__totalTermAndDoc = termAndDoc
+    def __init__(self, pathData, pathID, pathQuery, uniqueT, isStemming):
         self.__uniqueTerm = uniqueT
-        self.__ID = ID_mapping
+        self.__buildData(pathData)
+        self.__buildID(pathID)
+        if isStemming:
+            self.__stemmer = PorterStemmer()
+        self.__buildQuery(pathQuery, isStemming)
+
+
+    def __buildData(self, pathData):
+        # read data
+        self.__index = {}
+        self.__lenDict = {}
+        self.__totalTermAndDoc = []
+
+        isHead = 1
+        nowTerm = ""
+        with open(pathData) as file:
+            for line in file:
+                if isHead == 1:
+                    self.__totalTermAndDoc = [int(i) for i in line[:-1].split(" ")]
+                    isHead = 0
+                else:
+                    if line[0] != "\t":
+                        temp = line[:-1].split(" ")
+                        self.__index[temp[0]] = [[int(i) for i in temp[1:]]]
+                        nowTerm = temp[0]
+                    else:
+                        temp = line[1:-1].split(" ")
+                        toInput = [temp[0], int(temp[1])]  # 0: doc ID, 1: appear times
+                        self.__index[nowTerm].append(toInput)
+
+                        if toInput[0] not in self.__lenDict:
+                            self.__lenDict[toInput[0]] = int(toInput[1])
+                        else:
+                            self.__lenDict[toInput[0]] += int(toInput[1])
+
+    def __buildID(self, pathID):
+        # read ID map
+        self.__ID = []
+        with open(pathID) as file:
+            for line in file:
+                temp = line[:-1].split(" ")
+                self.__ID.append(temp)
+
+    @staticmethod
+    def __clean(queryString):
+        queryString = queryString.replace(",", "")
+        # queryString = queryString.replace("\s+", " ")
+        queryString = queryString.lower()
+        output = queryString.split(" ")
+
+        return output
+
+    def __stem(self, queryWordList):
+        return [self.__stemmer.stem(word, 0, len(word) - 1) for word in queryWordList]
+
+    def __buildQuery(self, pathQuery, isStemming):
+        self.__query = []
+        temp = [0] * 2
+        with open(pathQuery) as file:
+            for line in file:
+                if line[:5] == "<num>":
+                    first = line.find(":") + 2
+                    temp[0] = int(line[first:first + 3])
+                elif line[:7] == "<title>":
+                    first = line.find(">") + 2
+                    if line.find(" \n") == -1:
+                        last = -1
+                    else:
+                        last = -2
+                    temp[1] = line[first:last]
+                    self.__query.append(temp.copy())
+
+        for i in range(len(self.__query)):
+            self.__query[i][1] = self.__clean(self.__query[i][1])
+            if isStemming:
+                self.__query[i][1] = self.__stem(self.__query[i][1])
 
     def __VectorSpace(self, query):
         # Vector space model
@@ -33,7 +113,8 @@ class Model:
                 OkapiTF = tfOfDoc / (tfOfDoc + 0.5 + 1.5 * self.__lenDict[docID] / averageL)
                 outputVS[docID][i] = OkapiTF * math.log(self.__totalTermAndDoc[1] / (nKey - 1))
 
-            queryTFIDF[i] = 1 / (1 + 0.5 + 1.5 * len(query) / averageL) * math.log(self.__totalTermAndDoc[1] / (nKey - 1))
+            queryTFIDF[i] = 1 / (1 + 0.5 + 1.5 * len(query) / averageL) * math.log(
+                self.__totalTermAndDoc[1] / (nKey - 1))
 
         for key, value in outputVS.items():
             temp = [key, np.dot(value, queryTFIDF)]
@@ -98,23 +179,23 @@ class Model:
 
         return listLMJM
 
-    def printVectorSpace(self, query):
-        for subQuery in range(len(query)):
-            VSList = self.__VectorSpace(query[subQuery])
+    def printVectorSpace(self):
+        for subQuery in range(len(self.__query)):
+            VSList = self.__VectorSpace(self.__query[subQuery][1])
 
             if len(VSList) < 1000:
-                printIndex = len(VSList)
+                    printIndex = len(VSList)
             else:
                 printIndex = 1000
 
             for doc in range(1, printIndex + 1):
-                print("%d Q0 %s %d %f Exp" % (subQuery+1, self.__ID[int(VSList[doc - 1][0]) - 1][1], doc, VSList[doc - 1][1]))
+                print("%d Q0 %s %d %f Exp" % (self.__query[subQuery][0], self.__ID[int(VSList[doc - 1][0]) - 1][1], doc, VSList[doc - 1][1]))
 
             print("\n")
 
-    def printLanguageModelLaplace(self, query):
-        for subQuery in range(len(query)):
-            LaplaceList = self.__LanguageModelLaplace(query[subQuery])
+    def printLanguageModelLaplace(self):
+        for subQuery in range(len(self.__query)):
+            LaplaceList = self.__LanguageModelLaplace(self.__query[subQuery][1])
 
             if len(LaplaceList) < 1000:
                 printIndex = len(LaplaceList)
@@ -122,13 +203,13 @@ class Model:
                 printIndex = 1000
 
             for doc in range(1, printIndex + 1):
-                print("%d Q0 %s %d %f Exp" % (subQuery+1, self.__ID[int(LaplaceList[doc - 1][0]) - 1][1], doc, LaplaceList[doc - 1][1]))
+                print("%d Q0 %s %d %f Exp" % (self.__query[subQuery][0], self.__ID[int(LaplaceList[doc - 1][0]) - 1][1], doc, LaplaceList[doc - 1][1]))
 
             print("\n")
 
-    def printLanguageModelJM(self, query):
-        for subQuery in range(len(query)):
-            JMList = self.__LanguageModelJM(query[subQuery])
+    def printLanguageModelJM(self):
+        for subQuery in range(len(self.__query)):
+            JMList = self.__LanguageModelJM(self.__query[subQuery][1])
 
             if len(JMList) < 1000:
                 printIndex = len(JMList)
@@ -136,13 +217,6 @@ class Model:
                 printIndex = 1000
 
             for doc in range(1, printIndex + 1):
-                print("%d Q0 %s %d %f Exp" % (subQuery+1, self.__ID[int(JMList[doc - 1][0]) - 1][1], doc, JMList[doc - 1][1]))
+                print("%d Q0 %s %d %f Exp" % (self.__query[subQuery][0], self.__ID[int(JMList[doc - 1][0]) - 1][1], doc, JMList[doc - 1][1]))
 
             print("\n")
-
-    @staticmethod
-    def queryPreprocess(query):
-
-
-
-        return
--- Project2/mainP2.py
+++ Project2/mainP2.py
@@ -2,57 +2,23 @@ from Models import Model
 
 
 def main():
-
     # changeable
-    uniqueTerm = 27439  # 不在檔案中
     pathData = "data/toTest.txt"
     pathID = "data/ID/ID_toTest.txt"
-
-    # read data
-    inputData = {}
-    lenDict = {}
-    totalTermAndDoc = []
-
-    isHead = 1
-    nowTerm = ""
-    with open(pathData) as file:
-        for line in file:
-            if isHead == 1:
-                totalTermAndDoc = [int(i) for i in line[:-1].split(" ")]
-                isHead = 0
-            else:
-                if line[0] != "\t":
-                    temp = line[:-1].split(" ")
-                    inputData[temp[0]] = [[int(i) for i in temp[1:]]]
-                    nowTerm = temp[0]
-                else:
-                    temp = line[1:-1].split(" ")
-                    toInput = [temp[0], int(temp[1])]  # 0: doc ID, 1: appear times
-                    inputData[nowTerm].append(toInput)
-
-                    if toInput[0] not in lenDict:
-                        lenDict[toInput[0]] = int(toInput[1])
-                    else:
-                        lenDict[toInput[0]] += int(toInput[1])
-
-    # read ID map
-    ID_mapping = []
-    with open(pathID) as file:
-        for line in file:
-            temp = line[:-1].split(" ")
-            ID_mapping.append(temp)
-
-    # query not yet
-    query = [["blood", "believe", "me"], ["permanent", "surface"]]
+    pathQuery = "data/Query/toTest_query.txt"
+    uniqueTerm = 27439  # 不在檔案中
+    isStemming = False
 
     # declare model
-    MD = Model(inputData, lenDict, totalTermAndDoc, uniqueTerm, ID_mapping)
+    MD = Model(pathData=pathData, pathID=pathID, pathQuery=pathQuery, uniqueT=uniqueTerm, isStemming=isStemming)
+
     print("VectorSpace\n")
-    MD.printVectorSpace(query)
+    MD.printVectorSpace()
     print("Laplace\n")
-    MD.printLanguageModelLaplace(query)
+    MD.printLanguageModelLaplace()
     print("JM\n")
-    MD.printLanguageModelJM(query)
+    MD.printLanguageModelJM()
+
 
 if __name__ == '__main__':
     main()
