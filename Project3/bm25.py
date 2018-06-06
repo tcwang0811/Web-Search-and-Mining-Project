d52a54d2f8b4c7fdacf988b25ad1ce00388d820e
tcwang0811 <tcwang0811@gmail.com>
2018-06-06 15:06:21 +0800

    update comment

--- Project3/bm25.py
+++ Project3/bm25.py
@@ -12,12 +12,15 @@ def bm25(corpus, b, k1, stopword):
     aveL = output.sum()/output.shape[0]
     denominator = k1 * ((1-b)+b*(output.sum(1)/aveL))
     
+	# set elements of every row to k1*((1-b)+b*(docL/aveL))
     temp.data = temp.data/temp.data
     temp = csr_matrix.multiply(temp,denominator)
     
+	# to tf + k1*((1-b)+b*(docL/aveL))
     temp += output
     output *= (k1+1)
-
+	
+	# reciprocal and then multiply
     temp.data = 1/temp.data
     output = csr_matrix.multiply(output,temp)
     
